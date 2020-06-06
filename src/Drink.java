06ea8ed25e72610ef7b0f6d61d2772d6bffd1538
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-06 20:24:25 +0200

    Create IngredientCharacteristic class. Changes in Drink.java file.

--- src/Drink.java
+++ src/Drink.java
@@ -6,11 +6,3 @@ class Drink {
         String descriptionInfo;
         String description;
 }
-class IngredientCharacteristic {
-        String ingredient1;
-        String ingredient2;
-        String ingredient3;
-        String ingredientName;
-        int ingredientQuantity;
-        String ingredientVolume;
-}
---
+++ src/IngredientCharacteristic.java
@@ -0,0 +1,8 @@
+class IngredientCharacteristic {
+    String ingredient1;
+    String ingredient2;
+    String ingredient3;
+    String ingredientName;
+    int ingredientQuantity;
+    String ingredientVolume;
+}
